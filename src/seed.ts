987e9745d0f9899a47f205f6dba9f5d87919aead
Micha Buchholz <mail@lean-stack.de>
2023-01-18 08:48:06 +0100

    Add seed to with some mocked data

---
+++ src/seed.ts
@@ -0,0 +1,6 @@
+export const todosSeed = [
+  { id: 1, title: 'Context API', completed: false },
+  { id: 2, title: 'Toolchain', completed: true },
+  { id: 3, title: 'Component State', completed: true },
+  { id: 1, title: 'Custom Hooks', completed: false },
+];
